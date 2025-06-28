bcb26010767d423f8199f401974ccf79836662be
PHChemin <chemin_pedro@outlook.com>
2025-06-27 22:24:46 -0300

    Add lang

---
+++ backend/lang/pt_BR/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Idioma para Paginação
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de idioma são usadas pela biblioteca de paginação
+    | para construir os links de paginação simples. Você é livre para alterá-los
+    | para qualquer coisa que deseje, a fim de personalizar suas views para
+    | combinar melhor com sua aplicação.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Próximo &raquo;',
+
+];
---
+++ backend/lang/pt_BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Idioma para Redefinição de Senha
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de idioma são as padrões que correspondem aos motivos
+    | fornecidos pelo intermediário de senha para uma tentativa de atualização
+    | de senha com falha devido a senha ou token de redefinição inválidos.
+    |
+    */
+
+    'reset' => 'Sua senha foi redefinida com sucesso.',
+    'sent' => 'Enviamos por e-mail o link para redefinição de sua senha.',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => "Não encontramos um usuário com este endereço de e-mail.",
+
+];
