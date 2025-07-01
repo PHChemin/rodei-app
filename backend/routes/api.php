580eedee09deb4cc12f4dfac7c149fecd147a703
PHChemin <chemin_pedro@outlook.com>
2025-07-01 19:52:25 -0300

    add, edit and delete truck routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Fleet\FleetController;
+use App\Http\Controllers\Truck\TruckController;
 use App\Http\Controllers\User\RegisterUserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/fleets', [FleetController::class, 'store'])->name('fleets.store');
     Route::put('/fleets/{fleet}', [FleetController::class, 'update'])->name('fleets.update');
     Route::delete('/fleets/{fleet}', [FleetController::class, 'destroy'])->name('fleets.destroy');
+
+    Route::post('/fleets/{fleet}/trucks', [TruckController::class, 'storeTruck'])->name('fleets.trucks.store');
+    Route::put('/fleets/{fleet}/trucks/{truck}', [TruckController::class, 'updateTruck'])->name('fleets.trucks.update');
+    Route::delete('/fleets/{fleet}/trucks/{truck}', [TruckController::class, 'destroyTruck'])->name('fleets.trucks.destroy');
 });
