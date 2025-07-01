f255509c6a54830ab63a6ba282eafc3116c74476
PHChemin <chemin_pedro@outlook.com>
2025-07-01 01:41:16 -0300

    Feature and unit test for the features

---
+++ backend/tests/Unit/Actions/Fleet/DeleteFleetActionTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit\Actions\Fleet;
+
+use App\Http\Actions\Fleet\DeleteFleetAction;
+use App\Models\Manager;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DeleteFleetActionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_should_delete_fleet(): void
+    {
+        $manager = Manager::factory()->create();
+        $fleet = $manager->fleets()->create(['name' => 'Frota para deletar']);
+
+        (new DeleteFleetAction($fleet))->execute();
+
+        $this->assertDatabaseMissing('fleets', ['id' => $fleet->id]);
+    }
+}
