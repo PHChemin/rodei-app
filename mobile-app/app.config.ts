166a69da4a0766274f3430e0cdf372ab59d94993
Pedro Henrique Chemin <134353389+PHChemin@users.noreply.github.com>
2025-09-08 17:16:15 -0300

    Chore: Expo Environment Variables (#28)
    * App configuration for environment variables
    
    * add expo-dev-client and show api url in login screen

---
+++ mobile-app/app.config.ts
@@ -0,0 +1,117 @@
+import { ConfigContext, ExpoConfig } from "expo/config";
+import { version } from "./package.json";
+
+// Replace these with your EAS project ID and project slug.
+// You can find them at https://expo.dev/accounts/[account]/projects/[project].
+const EAS_PROJECT_ID = "d2292a4f-a6e6-482f-a703-63668ef0502c";
+const PROJECT_SLUG = "rodei";
+const OWNER = "phchemin";
+
+// App production config
+const APP_NAME = "Rodei";
+const BUNDLE_IDENTIFIER = "com.phchemin.rodei";
+const PACKAGE_NAME = "com.phchemin.rodei";
+const ICON = "./assets/images/icon.png";
+const ADAPTIVE_ICON = "./assets/images/adaptive-icon.png";
+const SCHEME = "mobile-app";
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  //   console.log("⚙️ Building app for environment:", process.env.APP_ENV);
+  const { name, bundleIdentifier, icon, adaptiveIcon, packageName, scheme } =
+    getDynamicAppConfig(
+      (process.env.APP_ENV as "development" | "preview" | "production") ||
+        "development"
+    );
+
+  return {
+    ...config,
+    name: name,
+    version, // Automatically bump your project version with `npm version patch`, `npm version minor` or `npm version major`.
+    slug: PROJECT_SLUG, // Must be consistent across all environments.
+    orientation: "portrait",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    icon: icon,
+    scheme: scheme,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: bundleIdentifier,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: adaptiveIcon,
+        backgroundColor: "#ffffff",
+      },
+      package: packageName,
+    },
+    updates: {
+      url: `https://u.expo.dev/${EAS_PROJECT_ID}`,
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
+    extra: {
+      eas: {
+        projectId: EAS_PROJECT_ID,
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          resizeMode: "contain",
+          width: 200,
+          backgroundColor: "#082537",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    owner: OWNER,
+  };
+};
+
+// Dynamically configure the app based on the environment.
+// Update these placeholders with your actual values.
+export const getDynamicAppConfig = (
+  environment: "development" | "preview" | "production"
+) => {
+  if (environment === "production") {
+    return {
+      name: APP_NAME,
+      bundleIdentifier: BUNDLE_IDENTIFIER,
+      packageName: PACKAGE_NAME,
+      icon: ICON,
+      adaptiveIcon: ADAPTIVE_ICON,
+      scheme: SCHEME,
+    };
+  }
+
+  if (environment === "preview") {
+    return {
+      name: `${APP_NAME} Preview`,
+      bundleIdentifier: `${BUNDLE_IDENTIFIER}.preview`,
+      packageName: `${PACKAGE_NAME}.preview`,
+      icon: ICON,
+      adaptiveIcon: ADAPTIVE_ICON,
+      scheme: SCHEME,
+    };
+  }
+
+  return {
+    name: `${APP_NAME} Development`,
+    bundleIdentifier: `${BUNDLE_IDENTIFIER}.dev`,
+    packageName: `${PACKAGE_NAME}.dev`,
+    icon: ICON,
+    adaptiveIcon: ADAPTIVE_ICON,
+    scheme: SCHEME,
+  };
+};
