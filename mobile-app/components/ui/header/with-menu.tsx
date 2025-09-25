1e3aebf4bcce557f5480838bee71430e7e3705ea
PHChemin <chemin_pedro@outlook.com>
2025-09-25 17:16:02 -0300

    add different menu by role

--- mobile-app/components/ui/header/with-menu.tsx
+++ mobile-app/components/ui/header/with-menu.tsx
@@ -1,9 +1,15 @@
 import { Stack } from "expo-router";
 
-import { mainMenuItems } from "@/menus/mainMenuItems";
+import { useToken } from "@/hooks/use-token";
+import { getMenuItems } from "@/menus/mainMenuItems";
+
 import HeaderMenu from "./header-menu";
 
 export function WithMenu() {
+  const { user } = useToken();
+
+  const mainMenuItems = getMenuItems(user!.is_manager);
+
   return (
     <Stack.Screen
       options={{
--- mobile-app/menus/mainMenuItems.ts
+++ mobile-app/menus/mainMenuItems.ts
@@ -1,41 +1,30 @@
 import { HeaderMenuItemProps } from "@/components/ui/header/header-menu";
-import pb from "@/services/pb";
 import { logout } from "@/services/user/logout";
 import { router } from "expo-router";
 
-export const mainMenuItems: HeaderMenuItemProps[] = [
+// Routes that are common to both roles
+const userMenu: HeaderMenuItemProps[] = [
   {
-    icon: {
-      type: "antdesign",
-      name: "home",
-    },
-    title: "Início",
-    onPress: () => router.push("/(auth)/home"),
-  },
-  {
-    icon: {
-      type: "antdesign",
-      name: "user",
-    },
+    icon: { type: "antdesign", name: "user" },
     title: "Meu Perfil",
     onPress: () => router.push("/(auth)/profile"),
   },
   {
-    icon: {
-      type: "antdesign",
-      name: "message1",
-    },
+    icon: { type: "antdesign", name: "message1" },
     title: "Opine sobre o app!",
     onPress: () => router.push("/(auth)/user-feedback"),
   },
   {
-    icon: {
-      type: "antdesign",
-      name: "logout",
-    },
+    icon: { type: "antdesign", name: "logout" },
     title: "Sair",
-    onPress: () => {
-      logout();
-    },
+    onPress: logout,
   },
 ];
+
+const managerMenu: HeaderMenuItemProps[] = [...userMenu];
+
+const driverMenu: HeaderMenuItemProps[] = [...userMenu];
+
+export function getMenuItems(isManager: boolean) {
+  return isManager ? managerMenu : driverMenu;
+}
