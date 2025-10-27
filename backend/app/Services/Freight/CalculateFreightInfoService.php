8feb63e3f68c64cb7fd4473f7ad20290b990c22d
PHChemin <chemin_pedro@outlook.com>
2025-10-27 03:05:33 -0300

    Add driver commission to FreightFactory, Add calculate expenses and profit to freight service and add tests

--- backend/app/Services/Freight/CalculateFreightInfoService.php
+++ backend/app/Services/Freight/CalculateFreightInfoService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\Freight;
 
+use App\Models\Expense;
+
 class CalculateFreightInfoService
 {
     public static function calculateAdvance(float|int $totalAmount, float|int $percentage): float|int
@@ -12,5 +14,15 @@ class CalculateFreightInfoService
     public static function calculateDriverCommission(float|int $totalAmount, float|int $commissionPercentage): float|int
     {
         return round($totalAmount * ($commissionPercentage / 100), 2);
+    }
+
+    public static function calculateExpenses(int $freightId): float
+    {
+        return Expense::where('freight_id', $freightId)->sum('amount');
+    }
+
+    public static function calculateProfit(float|int $freightAmount, float|int $expensesAmount, float|int $driverCommission): float
+    {
+        return round($freightAmount - $expensesAmount - $driverCommission, 2);
     }
 }
--- backend/database/factories/FreightFactory.php
+++ backend/database/factories/FreightFactory.php
@@ -23,6 +23,7 @@ class FreightFactory extends Factory
         $fleet = Fleet::factory()->create();
         $driver = Driver::factory()->create();
         $truck = Truck::factory()->for($fleet)->for($driver)->create();
+        $totalAmount = fake()->randomFloat(2, 0);
 
         return [
             'start_address' => fake()->address(),
@@ -33,7 +34,8 @@ class FreightFactory extends Factory
             'ton_price' => fake()->randomFloat(2, 0),
             'advance_percentage' => fake()->randomFloat(2, 0, 100),
             'advance' => CalculateFreightInfoService::calculateAdvance(fake()->randomFloat(2, 0), fake()->randomFloat(2, 0, 100)),
-            'total_amount' => fake()->randomFloat(2, 0),
+            'driver_commission' => CalculateFreightInfoService::calculateDriverCommission($totalAmount, $truck->commission_percentage),
+            'total_amount' => $totalAmount,
             'description' => fake()->text(),
             'fleet_id' => $fleet->id,
             'truck_id' => $truck->id,
