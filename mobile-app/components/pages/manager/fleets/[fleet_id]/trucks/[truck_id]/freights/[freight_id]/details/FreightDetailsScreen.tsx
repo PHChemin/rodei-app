ea58b17978d16ec6ff91304ec7d4da0f0aabc609
PHChemin <chemin_pedro@outlook.com>
2025-10-27 15:07:59 -0300

    Add expenses history to FreightDetailsScreen and add formated currency values

--- mobile-app/components/pages/manager/fleets/[fleet_id]/trucks/[truck_id]/freights/[freight_id]/details/FreightDetailsScreen.tsx
+++ mobile-app/components/pages/manager/fleets/[fleet_id]/trucks/[truck_id]/freights/[freight_id]/details/FreightDetailsScreen.tsx
@@ -1,8 +1,13 @@
 import { makeStyles } from "@rneui/themed";
 import { t } from "i18next";
 
+import { useAsyncData } from "@/hooks/use-async-data";
+import { FreightDetailsWithExpensesSchema } from "@/schemas";
+import { api } from "@/services";
+
 import { Header, ScreenWrapper } from "@/components/ui";
 
+import { ExpensesHisotry } from "./_expenses-history/_ExpensesHistory";
 import { FreightDetails } from "./_FreightDetails";
 
 type FreightDetailsScreenProps = {
@@ -18,16 +23,33 @@ export function FreightDetailsScreen({
 }: FreightDetailsScreenProps) {
   const styles = useStyles();
 
+  const { loading, freight } = useAsyncData(async () => {
+    const { data } = await api().get(
+      `/fleets/${fleetId}/trucks/${truckId}/freights/${freightId}`
+    );
+
+    const freight = FreightDetailsWithExpensesSchema.parse(data.data);
+
+    return {
+      freight,
+    };
+  });
+
+  if (loading) return loading;
+
   return (
-    <ScreenWrapper.Fullscreen>
+    <ScreenWrapper.Scrollable>
       <Header.WithTitle title={t("components.freight-details.title")} />
 
-      <FreightDetails
+      <FreightDetails freight={freight} />
+
+      <ExpensesHisotry
+        expenses={freight.expenses}
         fleetId={fleetId}
         truckId={truckId}
         freightId={freightId}
       />
-    </ScreenWrapper.Fullscreen>
+    </ScreenWrapper.Scrollable>
   );
 }
 
